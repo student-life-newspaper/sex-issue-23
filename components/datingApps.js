a6d37930c92ba6d30785061edfafaf88189a8236
Paige-Steuber <64327507+Paige-Steuber@users.noreply.github.com>
2023-02-10 17:29:27 -0600

    maybe

---
+++ components/datingApps.js
@@ -0,0 +1,177 @@
+class DatingApp extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+        <DatingApp>
+        <div class="container">
+        <h1>
+            Which year has had the most sex?
+        </h1>
+        <div class="row gy-5">
+            <div class="col-6">
+                <div class="circle-wrap tinder">
+                    <div class="circle">
+
+                        <div class="mask full">
+                            <div class="fill"></div>
+                        </div>
+
+                        <div class="mask half">
+                            <div class="fill"></div>
+                        </div>
+
+                        <div class="inside-circle">
+                            <h2>Tinder </h2> 
+                            <p class="percent">54.2%</p>
+                            
+
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="circle-wrap hinge">
+                    <div class="circle">
+
+                        <div class="mask full">
+                            <div class="fill"></div>
+                        </div>
+
+                        <div class="mask half">
+                            <div class="fill"></div>
+                        </div>
+
+                        <div class="inside-circle">
+                            <h2> Hinge</h2>
+
+                            <p class="percent">32.39%</p>
+                          
+                             
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="row gy-5">
+            <div class="col-6">
+                <div class="circle-wrap None">
+                    <div class="circle">
+
+                        <div class="mask full">
+                            <div class="fill"></div>
+                        </div>
+
+                        <div class="mask half">
+                            <div class="fill"></div>
+                        </div>
+
+                        <div class="inside-circle">
+                            <h2>Junior</h2>
+                            <p class="percent">32.07%</p>
+                            <p class="responses">276 responses</p>
+                             
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="col-6">
+                <div class="circle-wrap Bumble">
+                    <div class="circle">
+
+                        <div class="mask full">
+                            <div class="fill"></div>
+                        </div>
+
+                        <div class="mask half">
+                            <div class="fill"></div>
+                        </div>
+
+                        <div class="inside-circle">
+                            <h2>Bumble</h2>
+
+                            <p class="percent">30.21%</p>
+                           
+                            
+
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+        <div class="row gy-5">
+            <div class="col-6">
+                <div class="circle-wrap snapchat">
+                    <div class="circle">
+
+                        <div class="mask full">
+                            <div class="fill"></div>
+                        </div>
+
+                        <div class="mask half">
+                            <div class="fill"></div>
+                        </div>
+
+                        <div class="inside-circle">
+                            <h2>Snapchat</h2>
+                            <p class="percent">16.88%</p>
+                            
+                             
+                           
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="col-6">
+                <div class="circle-wrap DataMatc">
+                    <div class="circle">
+
+                        <div class="mask full">
+                            <div class="fill"></div>
+                        </div>
+
+                        <div class="mask half">
+                            <div class="fill"></div>
+                        </div>
+
+                        <div class="inside-circle">
+                           
+                            <h2>DataMatch</h2>
+                            
+                            <p class="percent">13%</p>
+                           
+
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        
+
+    </div>
+        </DatingApp>
+      `;
+    }
+  }
+  
+  customElements.define('dating-component', DatingApp);
--- js/script.js
+++ js/script.js
@@ -9,19 +9,73 @@ window.addEventListener("scroll", scrollEventHandler, false);
 const pages = [1, 2]
 let currentPage = 1;
 let btn = document.getElementById('next_btn');
+let prevbtn = document.getElementById('prev_btn');
+
 let body = document.getElementById('bod');
 
 btn.onclick = function(){
     currentPage += 1;
     console.log(currentPage)
  
-    changePage();
+    changePageForward();
 
 };
 
-function changePage(){
+prevbtn.onclick = function(){
+    if(currentPage == 1){
+        // alert("no pages")
+    }else{
+        currentPage -= 1;
+        console.log(currentPage)
+     
+        changePageBackward();
+    
+    }
+    
+};
+
+function changePageForward(){
     // let p = document.createElement("schoolsex-component");
+    
     if(currentPage == 2){
+       
+
+        prevbtn.setAttribute("class", "btn mybtn me-md-2")
+        let p = document.createElement("schoolsex-component");
+        p.id="newID"
+        
+        bod.append(p)
+       fadeIn()
+    }
+    else if(currentPage == 3){
+        // bod.style.opacity = 0;
+       p=document.getElementById("newID");
+    
+        p.remove();
+
+        let q = document.createElement("yearsex-component");
+        q.id="newID"
+        
+        bod.append(q)
+        // fadeIn()
+
+    }
+}
+
+function changePageBackward(){
+    // let p = document.createElement("schoolsex-component");
+    if(currentPage == 1){
+        let p =document.getElementById("newID");
+        p.remove();
+
+        prevbtn.setAttribute("class", "btn mybtn me-md-2 disabled")
+
+    }
+    else if(currentPage == 2){
+        prevbtn.setAttribute("class", "btn mybtn me-md-2")
+        let q=document.getElementById("newID");
+    
+        q.remove();
         let p = document.createElement("schoolsex-component");
         p.id="newID"
         
