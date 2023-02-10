ae65a5d11fc80184a304073fca70a894d947a324
Paige-Steuber <64327507+Paige-Steuber@users.noreply.github.com>
2023-02-10 12:02:34 -0600

    progress

--- components/footer.js
+++ components/footer.js
@@ -5,51 +5,6 @@ class Footer extends HTMLElement {
   
     connectedCallback() {
       this.innerHTML = `
-        <style>
-        footer{
-            position: absolute;
-            bottom: 0;
-            width: 100%;
-        }
-          nav{
-            height: 40px;
-           
-            display: flex;
-            align-items: center;
-            justify-content: left;
-            background-color:  pink;
-            
-           
-
-          }
-
-          .footer_ul{
-            height:0;
-          }
-  
-          ul {
-            padding: 0;
-            margin-top: 8px;
-            margin-bottom: auto;
-          }
-          
-          a {
-            font-weight: 700;
-            
-            margin: 0 25px;
-            color: #fff;
-            text-decoration: none;
-          }
-
-          footer a{
-            font-size: .6em;
-          }
-          
-          a:hover {
-            padding-bottom: 5px;
-            box-shadow: inset 0 -2px 0 0 #fff;
-          }
-        </style>
         <footer>
           <nav class="footer">
             <ul class="footer_ul">
--- components/header.js
+++ components/header.js
@@ -5,36 +5,6 @@ class Header extends HTMLElement {
   
     connectedCallback() {
       this.innerHTML = `
-        <style>
-        header{
-            height: 60px;
-        }
-          nav {
-            height: 40px;
-            display: flex;
-            align-items: center;
-            justify-content: left;
-            background-color:  #0a0a23;
-          }
-  
-          ul {
-            padding: 0;
-            margin-top: 8px;
-            margin-bottom: auto;
-          }
-          
-          a {
-            font-weight: 600;
-            margin: 0 25px;
-            color: #fff;
-            text-decoration: none;
-          }
-          
-          a:hover {
-            padding-bottom: 5px;
-            box-shadow: inset 0 -2px 0 0 #fff;
-          }
-        </style>
         <header>
           <nav>
             <ul>
