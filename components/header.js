2c2a2196c79c6bb07136fd7b4c839c7f005bc043
Paige-Steuber <64327507+Paige-Steuber@users.noreply.github.com>
2023-02-11 01:56:25 -0600

    comobo

--- components/header.js
+++ components/header.js
@@ -19,3 +19,26 @@ class Header extends HTMLElement {
   }
   
   customElements.define('header-component', Header);
+
+
+  class Footer extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+        <footer>
+          <nav class="footer">
+            <ul class="footer_ul">
+              <li><a href="#">Designed by Paige Steuber</a></li>
+            
+              
+            </ul>
+          </nav>
+        </footer>
+      `;
+    }
+  }
+  
+  customElements.define('footer-component', Footer);
